49fe7aa60050a7692e9465ac7b445f04f9c0a061
Arno Candel <arno.candel@gmail.com>
2023-03-27 09:11:33 -0700

    Allow export of gpt-j-6B + lora -> HF

--- export_hf_checkpoint.py
+++ export_hf_checkpoint.py
@@ -1,48 +1,54 @@
-import os
-import json
-
 import torch
-from peft import PeftModel, LoraConfig
+from peft import PeftModel
 
 import transformers
+from transformers import PreTrainedModel
+
+from finetune import get_loaders
 
 assert (
     "LlamaTokenizer" in transformers._import_structure["models.llama"]
 ), "LLaMA is now in HuggingFace's main branch.\nPlease reinstall it: pip uninstall transformers && pip install git+https://github.com/huggingface/transformers.git"
-from transformers import LlamaTokenizer, LlamaForCausalLM
 
-BASE_MODEL = None
-assert (
-    BASE_MODEL
-), "Please specify a BASE_MODEL in the script, e.g. 'decapoda-research/llama-7b-hf'"
+BASE_MODEL = 'EleutherAI/gpt-j-6B'
+OUTPUT_NAME = "gpt-j-6B.DAIdocs"
+llama_type = False
 
-tokenizer = LlamaTokenizer.from_pretrained(BASE_MODEL)
+model_loader, _ = get_loaders(llama_type=llama_type)
 
-base_model = LlamaForCausalLM.from_pretrained(
+base_model = model_loader.from_pretrained(
     BASE_MODEL,
     load_in_8bit=False,
     torch_dtype=torch.float16,
     device_map={"": "cpu"},
 )
 
-first_weight = base_model.model.layers[0].self_attn.q_proj.weight
+if llama_type:
+    layers = base_model.model.model.layers
+    first_weight = layers[0].self_attn.q_proj.weight
+else:
+    layers = base_model.transformer.base_model.h
+    first_weight = layers[0].attn.q_proj.weight
 first_weight_old = first_weight.clone()
 
 lora_model = PeftModel.from_pretrained(
     base_model,
-    "tloen/alpaca-lora-7b",
+    "lora_6B_daidocs",
     device_map={"": "cpu"},
     torch_dtype=torch.float16,
 )
 
-lora_weight = lora_model.base_model.model.model.layers[0].self_attn.q_proj.weight
-
 assert torch.allclose(first_weight_old, first_weight)
 
 # merge weights
-for layer in lora_model.base_model.model.model.layers:
-    layer.self_attn.q_proj.merge_weights = True
-    layer.self_attn.v_proj.merge_weights = True
+if llama_type:
+    for layer in lora_model.base_model.model.model.layers:
+        layer.self_attn.q_proj.merge_weights = True
+        layer.self_attn.v_proj.merge_weights = True
+else:
+    for layer in lora_model.base_model.transformer.base_model.h:
+        layer.attn.q_proj.merge_weights = True
+        layer.attn.v_proj.merge_weights = True
 
 lora_model.train(False)
 
@@ -56,6 +62,9 @@ deloreanized_sd = {
     if "lora" not in k
 }
 
-LlamaForCausalLM.save_pretrained(
-    base_model, "./hf_ckpt", state_dict=deloreanized_sd, max_shard_size="400MB"
+PreTrainedModel.save_pretrained(
+    base_model,
+    OUTPUT_NAME,
+    state_dict=deloreanized_sd,
+    max_shard_size="5GB",
 )
