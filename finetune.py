dc83c6f0f1c87c67edfa86d3eca19e0880134019
Pascal Pfeiffer <pascal.pfeiffer@h2o.ai>
2023-03-28 22:00:03 +0000

    Merge branch 'main'

--- finetune.py
+++ finetune.py
@@ -241,6 +241,7 @@ def train(
             load_best_model_at_end=True if val_set_size > 0 else False,
             ddp_find_unused_parameters=False if ddp else None,
             group_by_length=group_by_length,
+            report_to="neptune",
         ),
         data_collator=transformers.DataCollatorForSeq2Seq(
             tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True
