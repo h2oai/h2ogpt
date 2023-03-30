4bbe8ca812b3881b4ffdab61a208d0631cc3516e
Arno Candel <arno.candel@gmail.com>
2023-03-29 20:00:50 -0700

    Unify export scripts, adapt to llama13b.

--- export_hf_checkpoint.py
+++ export_hf_checkpoint.py
@@ -1,3 +1,5 @@
+import os
+import json
 import torch
 from peft import PeftModel
 
@@ -16,6 +18,7 @@ LORA_WEIGHTS = "lora_6B_daidocs_alpaca_daifaq"
 LORA_WEIGHTS = "llama-13b-hf.config.json.20_epochs.5e2efef6a3d2af21f217dd86f9d89c262877dbe2.20230329-022308"
 OUTPUT_NAME = (BASE_MODEL + LORA_WEIGHTS).split("/")[-1]
 llama_type = "llama" in BASE_MODEL
+as_pytorch = True  # False -> HF
 
 model_loader, _ = get_loaders(llama_type=llama_type)
 
@@ -59,15 +62,99 @@ lora_model.train(False)
 assert not torch.allclose(first_weight_old, first_weight)
 
 lora_model_sd = lora_model.state_dict()
-deloreanized_sd = {
-    k.replace("base_model.model.", ""): v
-    for k, v in lora_model_sd.items()
-    if "lora" not in k
-}
 
-PreTrainedModel.save_pretrained(
-    base_model,
-    OUTPUT_NAME,
-    state_dict=deloreanized_sd,
-    max_shard_size="5GB",
-)
+if as_pytorch:
+    # https://huggingface.co/decapoda-research/llama-13b-hf#quantitative-analysis
+    params = {
+        "dim": 5120,
+        "multiple_of": 256,
+        "n_heads": 40,
+        "n_layers": 40,
+        "norm_eps": 1e-06,
+        "vocab_size": -1,
+    }
+    n_layers = params["n_layers"]
+    n_heads = params["n_heads"]
+    dim = params["dim"]
+    dims_per_head = dim // n_heads
+    base = 10000.0
+    inv_freq = 1.0 / (base ** (torch.arange(0, dims_per_head, 2).float() / dims_per_head))
+
+    def permute(w):
+        return (
+            w.view(n_heads, dim // n_heads // 2, 2, dim).transpose(1, 2).reshape(dim, dim)
+        )
+
+
+    def unpermute(w):
+        return (
+            w.view(n_heads, 2, dim // n_heads // 2, dim).transpose(1, 2).reshape(dim, dim)
+        )
+
+
+    def translate_state_dict_key(k):
+        k = k.replace("base_model.model.", "")
+        if k == "model.embed_tokens.weight":
+            return "tok_embeddings.weight"
+        elif k == "model.norm.weight":
+            return "norm.weight"
+        elif k == "lm_head.weight":
+            return "output.weight"
+        elif k.startswith("model.layers."):
+            layer = k.split(".")[2]
+            if k.endswith(".self_attn.q_proj.weight"):
+                return f"layers.{layer}.attention.wq.weight"
+            elif k.endswith(".self_attn.k_proj.weight"):
+                return f"layers.{layer}.attention.wk.weight"
+            elif k.endswith(".self_attn.v_proj.weight"):
+                return f"layers.{layer}.attention.wv.weight"
+            elif k.endswith(".self_attn.o_proj.weight"):
+                return f"layers.{layer}.attention.wo.weight"
+            elif k.endswith(".mlp.gate_proj.weight"):
+                return f"layers.{layer}.feed_forward.w1.weight"
+            elif k.endswith(".mlp.down_proj.weight"):
+                return f"layers.{layer}.feed_forward.w2.weight"
+            elif k.endswith(".mlp.up_proj.weight"):
+                return f"layers.{layer}.feed_forward.w3.weight"
+            elif k.endswith(".input_layernorm.weight"):
+                return f"layers.{layer}.attention_norm.weight"
+            elif k.endswith(".post_attention_layernorm.weight"):
+                return f"layers.{layer}.ffn_norm.weight"
+            elif k.endswith("rotary_emb.inv_freq") or "lora" in k:
+                return None
+            else:
+                print(layer, k)
+                raise NotImplementedError
+        else:
+            print(k)
+            raise NotImplementedError
+
+
+    new_state_dict = {}
+    for k, v in lora_model_sd.items():
+        new_k = translate_state_dict_key(k)
+        if new_k is not None:
+            if "wq" in new_k or "wk" in new_k:
+                new_state_dict[new_k] = unpermute(v)
+            else:
+                new_state_dict[new_k] = v
+
+    os.makedirs("./ckpt", exist_ok=True)
+
+    torch.save(new_state_dict, "./ckpt/consolidated.00.pth")
+
+    with open("./ckpt/params.json", "w") as f:
+        json.dump(params, f)
+else:
+    deloreanized_sd = {
+        k.replace("base_model.model.", ""): v
+        for k, v in lora_model_sd.items()
+        if "lora" not in k
+    }
+
+    PreTrainedModel.save_pretrained(
+        base_model,
+        OUTPUT_NAME,
+        state_dict=deloreanized_sd,
+        max_shard_size="5GB",
+    )
