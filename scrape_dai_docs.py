d53d2e97ad39d93d512d0ab89b55f2cbeb2286bc
Arno Candel <arno.candel@gmail.com>
2023-04-03 17:04:01 -0700

    Fix typo

--- scrape_dai_docs.py
+++ scrape_dai_docs.py
@@ -354,7 +354,7 @@ def test_prep_vicuna_instruct():
             elif conv['from'] == 'gpt':
                 FROM = '<bot>: '
             convo += f"{FROM}" + conv['value'] + "\n"
-        training_rows.append(convo)
+        training_rows.append(dict(input=convo))
     with open(filename + ".generate_human_bot.train_plain.json", "wt") as f:
         f.write(json.dumps(training_rows, indent=2))
 
