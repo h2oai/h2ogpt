55f403057d64f2dc057d8ec73a91e784ceded46b
Arno Candel <arno.candel@gmail.com>
2023-04-17 19:45:40 -0700

    Make links for base models.

--- models/create_model_cards.py
+++ models/create_model_cards.py
@@ -9,25 +9,25 @@ from transformers import AutoModelForCausalLM
         (
                 "h2ogpt-oasst1-256-20b",
                 "[GPT NeoX 20B](https://huggingface.co/EleutherAI/gpt-neox-20b)",
-                "[h2oai/openassistant_oasst1](https://huggingface.co/datasets/h2oai/openassistant_oasst1)",
+                "h2oai/openassistant_oasst1",
                 "https://huggingface.co/h2oai/h2ogpt-oasst1-256-20b/blob/main/gpt-neox-20b.openassistant_oasst1.json.1_epochs.5fc91911bc2bfaaf3b6c2de577c4b0ae45a07a4a.18.zip",
         ),
         (
                 "h2ogpt-oasst1-512-12b",
                 "EleutherAI/pythia-12b",
-                "[h2oai/openassistant_oasst1](https://huggingface.co/datasets/h2oai/openassistant_oasst1)",
+                "h2oai/openassistant_oasst1",
                 "https://huggingface.co/h2oai/h2ogpt-oasst1-512-12b/blob/main/pythia-12b.openassistant_oasst1.json.1_epochs.d45a9d34d34534e076cc6797614b322bd0efb11c.15.zip",
         ),
         (
                 "h2ogpt-oig-oasst1-256-12b",
                 "EleutherAI/pythia-12b-deduped",
-                "[h2oai/h2ogpt-oig-oasst1-instruct-cleaned-v1](https://huggingface.co/datasets/h2oai/h2ogpt-oig-oasst1-instruct-cleaned-v1)",
+                "h2oai/h2ogpt-oig-oasst1-instruct-cleaned-v1",
                 "https://huggingface.co/h2oai/h2ogpt-oig-oasst1-256-12b/blob/main/pythia-12b-deduped.h2ogpt-oig-oasst1-instruct-cleaned-v1.json.1_epochs.5fc91911bc2bfaaf3b6c2de577c4b0ae45a07a4a.17.zip",
         ),
         (
                 "h2ogpt-oig-oasst1-256-6.9b",
                 "EleutherAI/pythia-6.9b",
-                "[h2oai/h2ogpt-oig-oasst1-instruct-cleaned-v1](https://huggingface.co/datasets/h2oai/h2ogpt-oig-oasst1-instruct-cleaned-v1)",
+                "h2oai/h2ogpt-oig-oasst1-instruct-cleaned-v1",
                 "https://huggingface.co/h2oai/h2ogpt-oig-oasst1-256-6.9b/blob/main/pythia-6.9b.h2ogpt-oig-oasst1-instruct-cleaned-v1.json.1_epochs.5fc91911bc2bfaaf3b6c2de577c4b0ae45a07a4a.9.zip",
         ),
     ],
@@ -51,10 +51,10 @@ def test_create_model_cards(model_name, base_model, dataset, training_logs):
         content = content.replace("<<MODEL_SIZE>>", model_size[:-1])
 
         assert "<<BASE_MODEL>>" in content
-        content = content.replace("<<BASE_MODEL>>", base_model)
+        content = content.replace("<<BASE_MODEL>>", f"[{base_model}](https://huggingface.co/{base_model})")
 
         assert "<<DATASET>>" in content
-        content = content.replace("<<DATASET>>", dataset)
+        content = content.replace("<<DATASET>>", f"[{dataset}](https://huggingface.co/{dataset})")
 
         assert "<<MODEL_ARCH>>" in content
         content = content.replace("<<MODEL_ARCH>>", model_arch)
