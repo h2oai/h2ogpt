05c742a1a283bf307bc745e8f276b0678f0ed9a9
Jonathan C. McKinney <pseudotensor@gmail.com>
2023-04-18 17:40:06 -0700

    Add minor note

--- client_test.py
+++ client_test.py
@@ -7,6 +7,9 @@ python generate.py  --base_model=h2oai/h2ogpt-oig-oasst1-256-6.9b --chat=False -
 
 NOTE: For private models, add --use-auth_token=True
 
+NOTE: --infer_devices=True can be used for multi-GPU in case see problems with cuda:x cuda:y mismatches.
+Currently, this will force model to be on a single GPU
+
 Then run this client as:
 
 python client_test.py
