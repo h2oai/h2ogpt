5c4fda7a9c4ecbdb9fcc29d812c8ce3a888f92d9
Jonathan C. McKinney <pseudotensor@gmail.com>
2023-04-20 16:02:50 -0700

    make update script bit more automatic

--- spaces/chatbot/repo_to_spaces.sh
+++ spaces/chatbot/repo_to_spaces.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# start in h2ogpt repo
+# NOTE: start in h2ogpt repo base directory
+
+h2ogpt_hash=`git rev-parse HEAD`
+
 ln -sr generate.py h2o-logo.svg LICENSE stopping.py prompter.py finetune.py utils.py client_test.py requirements.txt spaces/chatbot/
 cd ..
 
@@ -14,6 +17,6 @@ cd ../../../h2ogpt-chatbot/
 mv generate.py app.py
 
 git add app.py h2o-logo.svg LICENSE stopping.py prompter.py finetune.py utils.py client_test.py requirements.txt
-git commit -m "Add application file and dependencies"
+git commit -m "Update with h2ogpt hash ${h2ogpt_hash}"
 # ensure write token used and login with git control: huggingface-cli login --token <HUGGINGFACE_API_TOKEN> --add-to-git-credential
 git push
