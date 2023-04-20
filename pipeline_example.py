78ef7a397dc0f0c3f9b6875c8acf39d6dddbbd6f
Jonathan C. McKinney <pseudotensor@gmail.com>
2023-04-20 14:48:43 -0700

    Update pipeline example to use h2ogpt model and set load_in_8bit=True for 12B model so fits onto 24GB GPU

--- pipeline_example.py
+++ pipeline_example.py
@@ -1,33 +1,33 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
 from h2oai_pipeline import H2OTextGenerationPipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer
 import textwrap as tr
 
-tokenizer = AutoTokenizer.from_pretrained("togethercomputer/GPT-NeoXT-Chat-Base-20B", padding_side="left")
-model = AutoModelForCausalLM.from_pretrained("togethercomputer/GPT-NeoXT-Chat-Base-20B", device_map="auto")
+tokenizer = AutoTokenizer.from_pretrained("h2oai/h2ogpt-oasst1-512-12b", padding_side="left")
+
+# 8-bit will use much less memory, so set to True if
+# e.g. with 512-12b load_in_8bit=True required for 24GB GPU
+# if have 48GB GPU can do load_in_8bit=False for more accurate results
+load_in_8bit = True
+# device_map = 'auto' might work in some cases to spread model across GPU-CPU, but it's not supported
+device_map = {"": 0}
+model = AutoModelForCausalLM.from_pretrained("h2oai/h2ogpt-oasst1-512-12b", torch_dtype=torch.float16,
+                                             device_map=device_map, load_in_8bit=load_in_8bit)
 
 generate_text = H2OTextGenerationPipeline(model=model, tokenizer=tokenizer)
 
-input = "Explain to me the difference between nuclear fission and fusion."
-outputs = generate_text(input, max_new_tokens=200)
+# generate
+outputs = generate_text("Why is drinking water so healthy?", return_full_text=True, max_new_tokens=100)
+
 for output in outputs:
     print(tr.fill(output['generated_text'], width=40))
 
 # Generated text should be similar to below.
 """
-Nuclear fusion and fission are two ways
-that energy can be released from atoms.
-While both processes involve the
-splitting of atoms, they differ in how
-they do so. In nuclear fission, atoms
-are split apart, or fission, when they
-are hit by neutrons that have been given
-additional energy. The splitting of the
-atom releases a lot of energy, as well
-as other particles, including other
-neutrons. In nuclear fusion, two or more
-atoms are combined together to form a
-single larger atom, releasing a lot of
-energy in the process. The fusion of two
-atoms into one is called a nuclear
-reaction.
+Drinking water is a healthy habit
+because it helps to keep your body
+hydrated and flush out toxins. It also
+helps to keep your digestive system
+running smoothly and can even help to
+prevent certain diseases.
 """
