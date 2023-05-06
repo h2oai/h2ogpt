48e24f195dc17982d6253449a02ecb8943135956
Jonathan C. McKinney <pseudotensor@gmail.com>
2023-05-05 23:09:22 -0700

    Reform chatbot client API code, but serialize=False doesn't work due to gr.state issue: https://github.com/gradio-app/gradio/issues/4081

--- tests/test_client_calls.py
+++ tests/test_client_calls.py
@@ -13,8 +13,25 @@ def run_client1():
 
     from client_test import test_client_basic
     res_dict = test_client_basic()
-    assert res_dict['instruction_nochat'] == 'Who are you?'
-    assert res_dict['iinput_nochat'] == ''
+    assert res_dict['prompt'] == 'Who are you?'
+    assert res_dict['iinput'] == ''
+    assert 'I am h2oGPT' in res_dict['response']
+
+
+def test_client_chat_nostream():
+    from tests.utils import call_subprocess_onetask
+    call_subprocess_onetask(run_client1)
+
+
+def run_client_chat_nostream():
+    from generate import main
+    main(base_model='h2oai/h2ogpt-oig-oasst1-512-6.9b', prompt_type='human_bot', chat=True,
+         stream_output=False, gradio=True, num_beams=1, block_gradio_exit=False)
+
+    from client_test import run_client_basic
+    res_dict = run_client_basic(prompt='Who are you?', prompt_type='human_bot', chat=True)
+    assert res_dict['prompt'] == 'Who are you?'
+    assert res_dict['iinput'] == ''
     assert 'I am h2oGPT' in res_dict['response']
 
 
@@ -30,8 +47,8 @@ def run_client_long():
          stream_output=False, gradio=True, num_beams=1, block_gradio_exit=False)
 
     with open("/home/jon/Downloads/Gatsby_PDF_FullText.txt") as f:
-        instruction_nochat = f.readlines()
+        prompt = f.readlines()
 
     from client_test import run_client_basic
-    res_dict = run_client_basic(instruction_nochat=instruction_nochat, prompt_type='plain')
+    res_dict = run_client_basic(prompt=prompt, prompt_type='plain')
     print(res_dict['response'])
