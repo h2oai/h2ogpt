e9c1e85e84fc6573e2de20d533b47c158e227b05
Sajith Ariyarathna <this@users.noreply.github.com>
2023-05-17 23:44:26 +0530

    Add async operation support for text completion

--- client/h2ogpt_client/core.py
+++ client/h2ogpt_client/core.py
@@ -1,3 +1,4 @@
+import asyncio
 from enum import Enum
 from typing import Optional
 
@@ -24,17 +25,20 @@ class Client:
         self._client = gradio_client.Client(
             src=server_url, hf_token=huggingface_token, verbose=False
         )
-        self._text_completion = TextCompletionClient(self)
+        self._text_completion = TextCompletion(self)
 
     @property
-    def text_completion(self) -> "TextCompletionClient":
+    def text_completion(self) -> "TextCompletion":
         return self._text_completion
 
     def _predict(self, *args, api_name: str) -> str:
         return self._client.submit(*args, api_name=api_name).result()
 
+    async def _predict_async(self, *args, api_name: str) -> str:
+        return await asyncio.wrap_future(self._client.submit(*args, api_name=api_name))
 
-class TextCompletionClient:
+
+class TextCompletion:
     def __init__(self, client: Client):
         self._client = client
 
@@ -89,6 +93,79 @@ class TextCompletionClient:
         chat_mode = False
 
         return self._client._predict(
+            you,
+            input,
+            system_pre_context,
+            stream_output,
+            prompt_type.value,
+            temperature,
+            top_p,
+            top_k,
+            beams,
+            max_output_length,
+            min_output_length,
+            early_stopping,
+            max_time,
+            repetition_penalty,
+            number_returns,
+            enable_sampler,
+            chat_mode,
+            prompt,
+            input_context_for_instruction,
+            api_name="/submit_nochat",
+        )
+
+    async def create_async(
+        self,
+        prompt: str,
+        prompt_type: PromptType = PromptType.PLAIN,
+        input_context_for_instruction: str = "",
+        enable_sampler=False,
+        temperature: float = 1.0,
+        top_p: float = 1.0,
+        top_k: int = 40,
+        beams: float = 1.0,
+        early_stopping: bool = False,
+        min_output_length: int = 0,
+        max_output_length: int = 128,
+        max_time: int = 180,
+        repetition_penalty: float = 1.07,
+        number_returns: int = 1,
+        input: str = "",
+        system_pre_context: str = "",
+    ) -> str:
+        """
+        Creates a new text completion.
+
+        :param prompt: The prompt to generate completions for.
+        :param prompt_type: Type of the prompt.
+        :param input_context_for_instruction: Input context for instruction.
+        :param enable_sampler: Enable or disable tje sampler, required for use of
+                temperature, top_p, top_k.
+        :param temperature: What sampling temperature to use, between 0 and 3.
+                Lower values will make it more focused and deterministic, but may lead
+                to repeat. Higher values will make the output more creative, but may
+                lead to hallucinations.
+        :param top_p: Cumulative probability of tokens to sample from.
+        :param top_k: Number of tokens to sample from.
+        :param beams: Number of searches for optimal overall probability.
+                Higher values uses more GPU memory and compute.
+        :param early_stopping: Whether to stop early or not in beam search.
+        :param min_output_length:
+        :param max_output_length:
+        :param max_time: Maximum time to search optimal output.
+        :param repetition_penalty:
+        :param number_returns:
+        :param input:
+        :param system_pre_context:
+        :return: response from the model
+        """
+        # Not exposed parameters.
+        you = ""  # empty when chat_mode is False
+        stream_output = False
+        chat_mode = False
+
+        return await self._client._predict_async(
             you,
             input,
             system_pre_context,
--- client/tests/test_client.py
+++ client/tests/test_client.py
@@ -1,8 +1,25 @@
+import os
+
+import pytest
+
 from h2ogpt_client import Client
 
 
+def create_client(server_url: str = "") -> Client:
+    server_url = server_url or os.getenv("H2OGPT_SERVER", "http://0.0.0.0:7860")
+    return Client(server_url)
+
+
 def test_answer():
-    client = Client("http://0.0.0.0:7860")
+    client = create_client()
     r = client.text_completion.create("Hello world")
+    assert r
+    print(r)
+
+
+@pytest.mark.asyncio
+async def test_answer_async():
+    client = create_client()
+    r = await client.text_completion.create_async("Hello world")
     assert r
     print(r)
