968947da3e28935b4cd67680ec4acec60c71c2bb
Jonathan C. McKinney <pseudotensor@gmail.com>
2023-05-28 00:43:56 -0700

    Add vicuna langchain test

--- tests/test_langchain_simple.py
+++ tests/test_langchain_simple.py
@@ -2,24 +2,57 @@ from tests.utils import wrap_test_forked
 
 
 @wrap_test_forked
-def test_langchain_simple():
+def test_langchain_simple_h2ogpt():
+    run_langchain_simple(base_model='h2oai/h2ogpt-oasst1-512-12b', prompt_type='human_bot')
+
+
+@wrap_test_forked
+def test_langchain_simple_vicuna():
+    run_langchain_simple(base_model='junelee/wizard-vicuna-13b', prompt_type='instruct_vicuna')
+
+
+def run_langchain_simple(base_model='h2oai/h2ogpt-oasst1-512-12b', prompt_type='human_bot'):
+    """
+    :param base_model:
+    :param prompt_type: prompt_type required for stopping support and correct handling of instruction prompting
+    :return:
+    """
     import torch
     from transformers import AutoModelForCausalLM, AutoTokenizer
     from h2oai_pipeline import H2OTextGenerationPipeline
 
-    model_name = 'h2oai/h2ogpt-oasst1-512-12b'
-    prompt_type = 'human_bot'  # prompt_type required for stopping support and correct handling of instruction prompting
+    model_name = base_model
+
+    from transformers import AutoConfig
+    config = AutoConfig.from_pretrained(base_model, use_auth_token=True,
+                                        trust_remote_code=True,
+                                        offload_folder="./")
+
+    llama_type_from_config = 'llama' in str(config).lower()
+    llama_type_from_name = "llama" in base_model.lower()
+    llama_type = llama_type_from_config or llama_type_from_name
+
+    if llama_type:
+        from transformers import LlamaForCausalLM, LlamaTokenizer
+        model_loader = LlamaForCausalLM
+        tokenizer_loader = LlamaTokenizer
+    else:
+        model_loader = AutoModelForCausalLM
+        tokenizer_loader = AutoTokenizer
+
     load_in_8bit = True
     n_gpus = torch.cuda.device_count() if torch.cuda.is_available else 0
     device = 'cpu' if n_gpus == 0 else 'cuda'
     device_map = {"": 0} if device == 'cuda' else "auto"
-    tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side="left")
-    model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16, device_map=device_map,
-                                                 load_in_8bit=load_in_8bit)
+    tokenizer = tokenizer_loader.from_pretrained(model_name, padding_side="left")
+
+    model = model_loader.from_pretrained(model_name, torch_dtype=torch.bfloat16, device_map=device_map,
+                                         load_in_8bit=load_in_8bit)
 
     gen_kwargs = dict(max_new_tokens=512, return_full_text=True, early_stopping=False)
     pipe = H2OTextGenerationPipeline(model=model, tokenizer=tokenizer, prompt_type=prompt_type, **gen_kwargs)
-    # below makes it listen only to our prompt removal, not built in prompt removal that is less general and not specific for our model
+    # below makes it listen only to our prompt removal,
+    # not built in prompt removal that is less general and not specific for our model
     pipe.task = "text2text-generation"
 
     # create llm for LangChain
@@ -45,4 +78,8 @@ def test_langchain_simple():
     chain_kwargs = dict(input_documents=docs, question=query)
     answer = chain(chain_kwargs)
     print(answer)
-    assert 'Einstein' in answer['output_text'] and "Newton" in answer['output_text']
+
+    if 'vicuna' in base_model:
+        assert 'Both Albert Einstein and Sir Isaac Newton were brilliant scientists' in answer['output_text'] and "Newton" in answer['output_text']
+    else:
+        assert 'Einstein was a genius who revolutionized physics' in answer['output_text'] and "Newton" in answer['output_text']
