1391206e80f656d2dde68c2c95de4d12fc6774e2
Achraf Merzouki <achraf.merzouki@h2o.ai>
2023-06-02 15:15:26 -0400

    make sure tests can reference the h2ogpt installed package

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,16 @@
 import os
+import sys
+import importlib.util
 
 
 def pytest_itemcollected(item):
     item.name = item.name + os.getenv("PYTEST_TEST_NAME", "")
+
+
+def pytest_sessionstart(session):
+    if not os.getenv("IS_PR_BUILD", None):
+        return
+    try:
+        sys.path.append(os.path.dirname(importlib.util.find_spec("h2ogpt").origin))
+    except AttributeError:
+        pass
