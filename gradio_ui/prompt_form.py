afd45789bd24afabf79d6ed553f1c2cbd695d38e
Jiri Puc <jiri.puc@h2o.ai>
2023-06-08 19:24:09 +0200

    input prompt - yellow border added + change in label text; css code moved to separate file

--- gradio_ui/prompt_form.py
+++ gradio_ui/prompt_form.py
@@ -3,17 +3,20 @@ import gradio as gr
 
 def make_prompt_form(kwargs):
     if kwargs['input_lines'] > 1:
-        instruction_label = "You (Shift-Enter or push Submit to send message, use Enter for multiple input lines)"
+        instruction_label = "press Shift-Enter or click Submit to send message, press Enter for multiple input lines"
     else:
-        instruction_label = "You (Enter or push Submit to send message, shift-enter for more lines)"
+        instruction_label = "press Enter or click Submit to send message, press Shift-Enter for more lines"
 
     with gr.Row():
         with gr.Column(scale=50):
             instruction = gr.Textbox(
                 lines=kwargs['input_lines'],
-                label=instruction_label,
+                label='Ask anything',
                 placeholder=kwargs['placeholder_instruction'],
+                info=instruction_label,
+                elem_id='prompt-form'
             )
+            instruction.style(container=True)
         with gr.Row():
             submit = gr.Button(value='Submit').style(full_width=False, size='sm')
             stop_btn = gr.Button(value="Stop").style(full_width=False, size='sm')
