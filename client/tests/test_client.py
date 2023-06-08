c481bd4fa60068593d9958c73bf1950593642c6f
Sajith Ariyarathna <this@users.noreply.github.com>
2023-06-08 20:37:26 +0530

    Add chat completion API

--- client/tests/test_client.py
+++ client/tests/test_client.py
@@ -23,3 +23,24 @@ async def test_text_completion_async():
     r = await client.text_completion.create_async("Hello world")
     assert r
     print(r)
+
+
+def test_chat_completion():
+    client = create_client()
+    chat_context = client.chat_completion.create()
+
+    chat1 = chat_context.chat("Hey!")
+    assert chat1["user"] == "Hey!"
+    assert chat1["gpt"]
+
+    chat2 = chat_context.chat("How are you?")
+    assert chat2["user"] == "How are you?"
+    assert chat2["gpt"]
+
+    chat3 = chat_context.chat("Have a good day")
+    assert chat3["user"] == "Have a good day"
+    assert chat3["gpt"]
+
+    chat_history = chat_context.chat_history()
+    assert chat_history == [chat1, chat2, chat3]
+    print(chat_history)
