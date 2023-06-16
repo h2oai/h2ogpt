9aca8cbc6b5c4ed4b24210f1883e04e24d3c3c9a
Jonathan C. McKinney <pseudotensor@gmail.com>
2023-06-16 12:59:14 -0700

    Downgrade chromadb for https://github.com/hwchase17/langchain/issues/5714

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -63,7 +63,9 @@ def test_cli_langchain_llamacpp(monkeypatch):
     print(all_generations)
     assert len(all_generations) == 1
     assert "pexels-evg-kowalievska-1170986_small.jpg" in all_generations[0]
-    assert "The cat is sitting on a window seat and looking out the window" in all_generations[0] or "staring out the window at the city skyline" in all_generations[0]
+    assert "The cat is sitting on a window seat and looking out the window" in all_generations[0] or \
+           "staring out the window at the city skyline" in all_generations[0] or \
+        "The cat is likely relaxing and enjoying" in all_generations[0]
 
 
 @pytest.mark.need_tokens
