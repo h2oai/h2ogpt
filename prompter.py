16551e87250cdb227055dddb266409f119498f3c
Arno Candel <arno.candel@gmail.com>
2023-06-30 10:15:40 -0700

    Add xgen models.

--- prompter.py
+++ prompter.py
@@ -39,6 +39,8 @@ prompt_type_to_model_name = {
         'h2oai/h2ogpt-gm-oasst1-en-2048-falcon-7b-v2',
         'h2oai/h2ogpt-gm-oasst1-en-2048-falcon-40b-v1',
         'h2oai/h2ogpt-gm-oasst1-en-2048-falcon-40b-v2',
+        'h2oai/h2ogpt-gm-oasst1-en-xgen-7b-8k',
+        'h2oai/h2ogpt-gm-oasst1-multilang-xgen-7b-8k',
     ],
     'prompt_answer_openllama': [
         'h2oai/h2ogpt-gm-oasst1-en-2048-open-llama-7b-preview-300bt',
