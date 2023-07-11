32f0f419fe2d089a2757faf6067d66a94f97ad67
Arno Candel <arno.candel@gmail.com>
2023-07-11 16:50:23 -0700

    Cleanup

--- tests/test_fine_tune_export_tgi.sh
+++ tests/test_fine_tune_export_tgi.sh
@@ -1,15 +1,20 @@
-export MODEL=h2ogpt-oasst1-falcon-7b-test
-export HF_PORT=5000
-export MODEL_NAME=`echo $MODEL | sed 's@/@_@g'`
+export DATA=h2oai/openassistant_oasst1_h2ogpt
+export BASE_MODEL=tiiuae/falcon-7b
+#export BASE_MODEL=openlm-research/open_llama_3b
 export CUDA_VISIBLE_DEVICES=0
 
-rm -rf $MODEL.lora
+export MODEL=model-test
+export MODEL_NAME=`echo $MODEL | sed 's@/@_@g'`
+export HF_PORT=1000
+
 
 # Train LoRA
-python finetune.py --data_path=h2oai/openassistant_oasst1_h2ogpt --base_model=tiiuae/falcon-7b --num_epochs=0.01 --output_dir=$MODEL.lora
+rm -rf $MODEL.lora
+python finetune.py --data_path=$DATA --base_model=$BASE_MODEL --num_epochs=0.01 --output_dir=$MODEL.lora
 
 # Merge LoRA, export model to $MODEL dir (via env var)
+rm -rf $MODEL
 python src/export_hf_checkpoint.py
 
 # Load model with TGI
-docker run --gpus all --shm-size 1g -e CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES -e TRANSFORMERS_CACHE="/.cache/" -p $HF_PORT:80 -v $HOME/.cache:/.cache/ -v $PWD/$MODEL:/data/$MODEL ghcr.io/huggingface/text-generation-inference:latest --model-id /data/$MODEL --max-input-length 2048 --max-total-tokens 4096 --max-stop-sequences 6 --sharded false
+docker run --gpus all --shm-size 1g -e CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES -e TRANSFORMERS_CACHE="/.cache/" -p $HF_PORT:80 -v $HOME/.cache:/.cache/ -v $MODEL:/$MODEL ghcr.io/huggingface/text-generation-inference:latest --model-id /$MODEL --max-input-length 2048 --max-total-tokens 4096 --max-stop-sequences 6 --sharded false
