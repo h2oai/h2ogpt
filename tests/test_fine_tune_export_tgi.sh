18f55f6f7cc0387cac562a808f526056ba3a2887
Arno Candel <arno.candel@gmail.com>
2023-07-17 11:56:50 -0700

    Use TGI 0.9.2

--- tests/test_fine_tune_export_tgi.sh
+++ tests/test_fine_tune_export_tgi.sh
@@ -6,7 +6,7 @@ export MODEL=model-test
 export MODEL_NAME=`echo $MODEL | sed 's@/@_@g'`
 export HF_PORT=1000
 #export TGI_VERSION=0.8.2  # works
-export TGI_VERSION=0.9.1  # fails
+export TGI_VERSION=0.9.2  # fails
 
 
 # Train LoRA
