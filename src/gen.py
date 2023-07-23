e09e732fb5c7aa17f0cd163f3d40b86deff124b0
Jonathan C. McKinney <pseudotensor@gmail.com>
2023-07-22 17:51:50 -0700

    Avoid transformers hack for TextStreamer for space handling, messes up LLaMa2

--- src/gen.py
+++ src/gen.py
@@ -2256,6 +2256,42 @@ class H2OTextIteratorStreamer(TextIteratorStreamer):
         with self.text_queue.mutex:
             self.text_queue.queue.clear()
 
+    def put(self, value):
+        """
+        Receives tokens, decodes them, and prints them to stdout as soon as they form entire words.
+        # same as base class, except remove hack w.r.t. text.rfind(" ") that ruins LLaMa2
+        """
+        if len(value.shape) > 1 and value.shape[0] > 1:
+            raise ValueError("TextStreamer only supports batch size 1")
+        elif len(value.shape) > 1:
+            value = value[0]
+
+        if self.skip_prompt and self.next_tokens_are_prompt:
+            self.next_tokens_are_prompt = False
+            return
+
+        # Add the new token to the cache and decodes the entire thing.
+        self.token_cache.extend(value.tolist())
+        text = self.tokenizer.decode(self.token_cache, **self.decode_kwargs)
+
+        # After the symbol for a new line, we flush the cache.
+        if text.endswith("\n"):
+            printable_text = text[self.print_len:]
+            self.token_cache = []
+            self.print_len = 0
+        # If the last token is a CJK character, we print the characters.
+        elif len(text) > 0 and self._is_chinese_char(ord(text[-1])):
+            printable_text = text[self.print_len:]
+            self.print_len += len(printable_text)
+        # Otherwise, prints until the last space char (simple heuristic to avoid printing incomplete words,
+        # which may change with the subsequent token -- there are probably smarter ways to do this!)
+        else:
+            # printable_text = text[self.print_len : text.rfind(" ") + 1]
+            printable_text = text[self.print_len:]
+            self.print_len += len(printable_text)
+
+        self.on_finalized_text(printable_text)
+
 
 def generate_with_exceptions(func, *args, prompt='', inputs_decoded='', raise_generate_gpu_exceptions=True, **kwargs):
     try:
