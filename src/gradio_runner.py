477cf41ced977a3237d0fa5fd9e3bc7237e07d57
Josh <jcatana@gmail.com>
2023-07-26 22:28:05 -0400

    Add additional param '--root_path' to from gradio Blocks.launch() arguments.

--- src/gradio_runner.py
+++ src/gradio_runner.py
@@ -2504,7 +2504,7 @@ def go_gradio(**kwargs):
 
     demo.launch(share=kwargs['share'], server_name="0.0.0.0", show_error=True,
                 favicon_path=favicon_path, prevent_thread_lock=True,
-                auth=kwargs['auth'])
+                auth=kwargs['auth'], root_path=kwargs['root_path'])
     if kwargs['verbose']:
         print("Started GUI", flush=True)
     if kwargs['block_gradio_exit']:
