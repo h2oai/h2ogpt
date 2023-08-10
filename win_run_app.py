a5a82b2be2efce02d84e521dbc6aa9914c581578
Jonathan C. McKinney <pseudotensor@gmail.com>
2023-08-09 22:14:26 -0700

    By default don't do score model

--- win_run_app.py
+++ win_run_app.py
@@ -30,7 +30,7 @@ for sub in ['src', 'iterators', 'gradio_utils', 'metrics', 'models', '.']:
 
 def main():
     from gen import main as main_h2ogpt
-    main_h2ogpt(block_gradio_exit=False)
+    main_h2ogpt(block_gradio_exit=False, score_model=None)
 
     url = "http://localhost:%s" % os.getenv('GRADIO_SERVER_PORT', str(7860))
     webbrowser.open(url)
