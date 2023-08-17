ad584bbd218567fa3fb5c4a62c698fe47c9ef364
chathurinda <chathurinda.ranasinghe@h2o.ai>
2023-08-17 19:06:37 +0530

    Add cuda11.7 for vLLM support & remove default model

--- cloud/packer/gcp/install_h2ogpt.sh
+++ cloud/packer/gcp/install_h2ogpt.sh
@@ -19,29 +19,6 @@ python3.10 -m pip install https://github.com/jllllll/exllama/releases/download/0
 
 sp=$(python3.10 -c 'import site; print(site.getsitepackages()[0])')
 sed -i 's/posthog\.capture/return\n            posthog.capture/' "$sp"/chromadb/telemetry/posthog.py
-sed -i 's/# n_gpu_layers=20/n_gpu_layers=20/g' .env_gpt4all
 
 sudo echo "export TRANSFORMERS_CACHE=~/.cache/huggingface/hub/" >> ~/.bashrc
 source ~/.bashrc
-wget --quiet https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/resolve/main/llama-2-7b-chat.ggmlv3.q8_0.bin
-
-cd /etc/systemd/system
-sudo chown -R ubuntu:ubuntu .
-
-printf """
-[Unit]
-Description=h2oGPT Service
-After=network.target
-[Service]
-Type=simple
-User=ubuntu
-WorkingDirectory=/home/ubuntu/h2ogpt
-ExecStart=/home/ubuntu/h2ogpt/venv/bin/python3.10 generate.py --base_model='llama' --prompt_type=llama2
-Restart=always
-[Install]
-WantedBy=multi-user.target
-""" >> h2ogpt.service
-
-sudo systemctl daemon-reload
-sudo systemctl enable h2ogpt.service
-sudo systemctl start h2ogpt.service
--- cloud/packer/gcp/setup_environment.sh
+++ cloud/packer/gcp/setup_environment.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+ #!/bin/bash -e
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y --no-install-recommends install \
@@ -37,9 +37,9 @@ source venv/bin/activate
 
 wget --quiet https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget --quiet https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda-repo-ubuntu2004-12-1-local_12.1.0-530.30.02-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu2004-12-1-local_12.1.0-530.30.02-1_amd64.deb
-sudo cp /var/cuda-repo-ubuntu2004-12-1-local/cuda-*-keyring.gpg /usr/share/keyrings/
+wget https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda-repo-ubuntu2004-11-7-local_11.7.0-515.43.04-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2004-11-7-local_11.7.0-515.43.04-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2004-11-7-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install cuda
 
