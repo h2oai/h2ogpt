ebf2c21895f4323e670e02604c4814e228d4696d
Ryan Chesler <Ryan.Chesler@h2o.ai>
2023-08-27 16:51:35 +0000

    removed make_interactive on file upload

--- src/gradio_runner.py
+++ src/gradio_runner.py
@@ -1276,14 +1276,16 @@ def go_gradio(**kwargs):
             return None
 
         def make_non_interactive(*args):
-            gr.update(interactive=False)
-            # if len(args) == 1:
-            #     return gr.update(interactive=False)
-            # else:
-            #     return tuple([gr.update(interactive=False)] * len(args))
+            if len(args) == 1:
+                return gr.update(interactive=False)
+            else:
+                return tuple([gr.update(interactive=False)] * len(args))
 
         def make_interactive(*args):
-            gr.update(interactive = True)
+            if len(args) == 1:
+                return gr.update(interactive=True)
+            else:
+                return tuple([gr.update(interactive=True)] * len(args))
 
         def set_loaders(max_quality1,
                         image_loaders_options1=None,
@@ -1348,8 +1350,8 @@ def go_gradio(**kwargs):
                                          show_progress='minimal')
         eventdb1 = eventdb1a.then(**add_file_kwargs, show_progress='full')
         
-        eventdb1b = eventdb1.then(make_interactive, inputs=add_file_outputs, outputs=[],
-                                  show_progress='minimal')
+        # eventdb1b = eventdb1.then(make_interactive, inputs=add_file_outputs, outputs=[],
+        #                           show_progress='minimal')
 
         # deal with challenge to have fileup_output itself as input
         add_file_kwargs2 = dict(fn=update_db_func,
@@ -1391,10 +1393,10 @@ def go_gradio(**kwargs):
                                     queue=queue,
                                     show_progress='minimal')
         # work around https://github.com/gradio-app/gradio/issues/4733
-        eventdb2b = eventdb2a.then(make_non_interactive, inputs=add_url_outputs, outputs=[],
+        eventdb2b = eventdb2a.then(make_non_interactive, inputs=add_url_outputs, outputs=add_url_outputs,
                                    show_progress='minimal')
         eventdb2 = eventdb2b.then(**add_url_kwargs, show_progress='full')
-        eventdb2c = eventdb2.then(make_interactive, inputs=add_url_outputs, outputs=[],
+        eventdb2c = eventdb2.then(make_interactive, inputs=add_url_outputs, outputs=add_url_outputs,
                                   show_progress='minimal')
 
         update_user_db_txt_func = functools.partial(update_db_func, is_txt=True)
@@ -1416,12 +1418,12 @@ def go_gradio(**kwargs):
                                           outputs=[my_db_state, requests_state, user_text_text],
                                           queue=queue,
                                           show_progress='minimal')
-        eventdb3b = eventdb3a.then(make_non_interactive, inputs=add_text_outputs, outputs=[],
+        eventdb3b = eventdb3a.then(make_non_interactive, inputs=add_text_outputs, outputs=add_text_outputs,
                                    show_progress='minimal')
         eventdb3 = eventdb3b.then(**add_text_kwargs, show_progress='full')
-        eventdb3c = eventdb3.then(make_interactive, inputs=add_text_outputs, outputs=[],
+        eventdb3c = eventdb3.then(make_interactive, inputs=add_text_outputs, outputs=add_text_outputs,
                                   show_progress='minimal')
-        db_events = [eventdb1a, eventdb1, eventdb1b, eventdb1_api,
+        db_events = [eventdb1a, eventdb1, eventdb1_api,
                      eventdb2a, eventdb2, eventdb2b, eventdb2c,
                      eventdb3a, eventdb3b, eventdb3, eventdb3c]
 
