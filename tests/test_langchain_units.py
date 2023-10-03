3b446c994cfc9423c0d3107cd29d6b65f31828b7
Jonathan C. McKinney <pseudotensor@gmail.com>
2023-10-03 14:28:25 -0700

    Update test

--- tests/test_langchain_units.py
+++ tests/test_langchain_units.py
@@ -955,28 +955,47 @@ def test_pdf_add(db_type, enable_pdf_ocr, enable_pdf_doctr, use_pymupdf, use_uns
                 test_file1 = os.path.join(location, name)
                 shutil.copy(test_file1, tmp_user_path)
                 test_file1 = os.path.join(tmp_user_path, name)
-            db, collection_name = make_db_main(persist_directory=tmp_persist_directory, user_path=tmp_user_path,
-                                               fail_any_exception=True, db_type=db_type,
-                                               use_pymupdf=use_pymupdf,
-                                               enable_pdf_ocr=enable_pdf_ocr,
-                                               enable_pdf_doctr=enable_pdf_doctr,
-                                               use_unstructured_pdf=use_unstructured_pdf,
-                                               use_pypdf=use_pypdf,
-                                               add_if_exists=False)
-            assert db is not None
-            docs = db.similarity_search("Suggestions")
+
             default_mode = use_pymupdf in ['auto', 'on'] and \
-                           use_pypdf in ['off', 'auto'] and \
-                           use_unstructured_pdf in ['off', 'auto'] and \
+                           use_pypdf in ['auto'] and \
+                           use_unstructured_pdf in ['auto'] and \
                            enable_pdf_doctr in ['off', 'auto'] and \
                            enable_pdf_ocr in ['off', 'auto']
+            no_doc_mode = use_pymupdf in ['off'] and \
+                           use_pypdf in ['off'] and \
+                           use_unstructured_pdf in ['off'] and \
+                           enable_pdf_doctr in ['off'] and \
+                           enable_pdf_ocr in ['off', 'auto']
+
+            try:
+                db, collection_name = make_db_main(persist_directory=tmp_persist_directory, user_path=tmp_user_path,
+                                                   fail_any_exception=True, db_type=db_type,
+                                                   use_pymupdf=use_pymupdf,
+                                                   enable_pdf_ocr=enable_pdf_ocr,
+                                                   enable_pdf_doctr=enable_pdf_doctr,
+                                                   use_unstructured_pdf=use_unstructured_pdf,
+                                                   use_pypdf=use_pypdf,
+                                                   add_if_exists=False)
+            except Exception as e:
+                if 'had no valid text and no meta data was parsed' in str(e) or 'had no valid text, but meta data was parsed' in str(e):
+                    if no_doc_mode:
+                        return
+                    else:
+                        raise
+                raise
+
+            assert db is not None
+            docs = db.similarity_search("Suggestions")
             if default_mode:
                 assert len(docs) == 3 + (1 if db_type == 'chroma' else 0)
             else:
                 # ocr etc. end up with different pages, overly complex to test exact count
                 assert len(docs) >= 2
             assert 'And more text. And more text.' in docs[0].page_content
-            assert os.path.normpath(docs[0].metadata['source']) == os.path.normpath(test_file1)
+            if db_type == 'weaviate':
+                assert os.path.normpath(docs[0].metadata['source']) == os.path.normpath(test_file1) or os.path.basename(docs[0].metadata['source']) == os.path.basename(test_file1)
+            else:
+                assert os.path.normpath(docs[0].metadata['source']) == os.path.normpath(test_file1)
 
 
 @pytest.mark.parametrize("use_pypdf", ['auto', 'on', 'off'])
