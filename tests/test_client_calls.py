75c235fd33ddf9ec14552959e33e70c671b422d7
Jonathan C. McKinney <pseudotensor@gmail.com>
2023-10-11 05:42:27 -0700

    Update test

--- tests/test_client_calls.py
+++ tests/test_client_calls.py
@@ -1075,6 +1075,7 @@ def test_client_chat_stream_langchain_steps3(loaders, enforce_h2ogpt_api_key):
     res_dict, client = run_client(client, prompt, args, kwargs)
     assert ('Yes, more text can be boring' in res_dict['response'] or
             "can be considered boring" in res_dict['response'] or
+            "the text in the provided PDF file is quite repetitive and boring" in res_dict['response'] or
             "the provided PDF file is quite boring" in res_dict['response']) \
            and 'sample1.pdf' in res_dict['response']
     # QUERY2
