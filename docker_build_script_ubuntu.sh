c4faa2fdcf8416347f7e5e7c967a6483dc78ab8a
Achraf Merzouki <achraf.merzouki@h2o.ai>
2023-10-12 18:50:04 -0400

    preload more model encodings

--- docker_build_script_ubuntu.sh
+++ docker_build_script_ubuntu.sh
@@ -63,7 +63,30 @@ from tiktoken_ext import openai_public
 # FakeTokenizer etc. needs tiktoken for general tasks
 for enc in openai_public.ENCODING_CONSTRUCTORS:
     encoding = tiktoken.get_encoding(enc)
-encoding = tiktoken.encoding_for_model('gpt-3.5-turbo')
+model_encodings = [
+    'gpt-4',
+    'gpt-4-0314',
+    'gpt-4-32k',
+    'gpt-4-32k-0314',
+    'gpt-3.5-turbo',
+    'gpt-3.5-turbo-16k',
+    'gpt-3.5-turbo-0301',
+    'text-ada-001',
+    'ada',
+    'text-babbage-001',
+    'babbage',
+    'text-curie-001',
+    'curie',
+    'davinci',
+    'text-davinci-003',
+    'text-davinci-002',
+    'code-davinci-002',
+    'code-davinci-001',
+    'code-cushman-002',
+    'code-cushman-001'
+]
+for enc in model_encodings:
+    encoding = tiktoken.encoding_for_model(enc)
 print('Done!')
 "
 
