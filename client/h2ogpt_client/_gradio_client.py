d8deb9ecab8c6e87ebaf3d4821b5ad733b5a3bbd
Sajith Ariyarathna <this@users.noreply.github.com>
2023-11-03 18:04:10 +0530

    Add output streaming support for text completion

--- client/h2ogpt_client/_gradio_client.py
+++ client/h2ogpt_client/_gradio_client.py
@@ -1,5 +1,6 @@
 import asyncio
-from typing import Any, Optional
+import time
+from typing import Any, AsyncGenerator, Generator, List, Optional
 
 import gradio_client  # type: ignore
 
@@ -19,5 +20,35 @@ class GradioClientWrapper:
     def predict(self, *args, api_name: str) -> Any:
         return self._client.predict(*args, api_name=api_name)
 
+    def predict_and_stream(self, *args, api_name: str) -> Generator[str, None, None]:
+        job = self._client.submit(*args, api_name=api_name)
+        while not job.done():
+            outputs: List[str] = job.outputs()
+            if not len(outputs):
+                time.sleep(0.1)
+                continue
+            newest_response = outputs[-1]
+            yield newest_response
+
+        e = job.exception()
+        if e and isinstance(e, BaseException):
+            raise RuntimeError from e
+
     async def submit(self, *args, api_name: str) -> Any:
         return await asyncio.wrap_future(self._client.submit(*args, api_name=api_name))
+
+    async def submit_and_stream(
+        self, *args, api_name: str
+    ) -> AsyncGenerator[Any, None]:
+        job = self._client.submit(*args, api_name=api_name)
+        while not job.done():
+            outputs: List[str] = job.outputs()
+            if not len(outputs):
+                await asyncio.sleep(0.1)
+                continue
+            newest_response = outputs[-1]
+            yield newest_response
+
+        e = job.exception()
+        if e and isinstance(e, BaseException):
+            raise RuntimeError from e
