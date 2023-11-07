681da921e1bdda25230f3193b0b722c5855f4dfd
Mathanraj-Sharma <rvmmathanraj@gmail.com>
2023-11-07 15:41:15 +0530

    Add configs related to MPS for one click installer

--- dev_installers/mac/build_mac_installer.sh
+++ dev_installers/mac/build_mac_installer.sh
@@ -6,6 +6,7 @@ then
     echo "conda could not be found, need conda to continue!"
     exit 1
 fi
+conda env remove -n h2ogpt-mac
 conda create -n h2ogpt-mac python=3.10 rust -y
 conda activate h2ogpt-mac
 
@@ -28,6 +29,19 @@ pip install https://h2o-release.s3.amazonaws.com/h2ogpt/hnswmiglib-0.7.0.tgz
 # Install PyInstaller
 pip install PyInstaller
 
+# For MPS support
+if [ -z "$BUILD_MPS" ]
+then
+    echo "BUILD_MPS is not set, skipping MPS specific configs..."
+else
+    if [ "$BUILD_MPS" = "1" ]
+    then
+        echo "BUILD_MPS is set to 1, running MPS specific configs..."
+        pip uninstall llama-cpp-python -y
+        CMAKE_ARGS="-DLLAMA_METAL=on" FORCE_CMAKE=1 pip install -U llama-cpp-python==0.2.11 --no-cache-dir
+    fi
+fi
+
 # Install and copy tesseract & poppler
 #brew install tesseract@5.3.3
 #brew install poppler@23.10.0
@@ -41,5 +55,12 @@ pip install ./dist/h2ogpt*.whl
 
 # Build Mac Installer
 # below command is used to build current .spec file replace it whenever use new configs
-# pyinstaller mac_run_app.py -F --name=h2ogpt-osx-m1-cpu --hiddenimport=h2ogpt --collect-all=h2ogpt --noconfirm --recursive-copy-metadata=transformers --collect-data=langchain --collect-data=gradio_client --collect-all=gradio --path=${CONDA_PREFIX}/python3.10/site-packages --collect-all=sentencepiece --add-data=./Tesseract-OCR:Tesseract-OCR --add-data=./poppler:poppler
-pyinstaller ./dev_installers/mac/h2ogpt-osx-m1-cpu.spec
+# pyinstaller mac_run_app.py -F --name=h2ogpt-osx-m1-cpu --hiddenimport=h2ogpt --collect-all=h2ogpt --noconfirm --recursive-copy-metadata=transformers --collect-data=langchain --collect-data=gradio_client --collect-all=gradio  --collect-all=sentencepiece --add-data=./Tesseract-OCR:Tesseract-OCR --add-data=./poppler:poppler
+if [ "$BUILD_MPS" = "1" ]
+then
+    echo "BUILD_MPS is set to 1, building one click installer for MPS..."
+    pyinstaller ./dev_installers/mac/h2ogpt-osx-m1-gpu.spec
+else
+    echo "BUILD_MPS is set to 0 or not set, building one click installer for CPU..."
+    pyinstaller ./dev_installers/mac/h2ogpt-osx-m1-cpu.spec
+fi
