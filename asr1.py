91f07280edd91ea7395f5abaeef543b387e4e094
Jonathan C. McKinney <pseudotensor@gmail.com>
2023-11-12 00:49:53 -0800

    asr examples

---
+++ asr1.py
@@ -0,0 +1,21 @@
+import gradio as gr
+from transformers import pipeline
+import numpy as np
+
+transcriber = pipeline("automatic-speech-recognition", model="openai/whisper-base.en")
+
+def transcribe(audio):
+    sr, y = audio
+    y = y.astype(np.float32)
+    y /= np.max(np.abs(y))
+
+    return transcriber({"sampling_rate": sr, "raw": y})["text"]
+
+
+demo = gr.Interface(
+    transcribe,
+    gr.Audio(sources=["microphone"]),
+    "text",
+)
+
+demo.launch()
