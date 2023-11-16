1485f9d25c99efe674f26c7d1a7f14d5e03c3536
Jonathan C. McKinney <pseudotensor@gmail.com>
2023-11-16 12:08:47 -0800

    Update test

--- tests/test_eval.py
+++ tests/test_eval.py
@@ -143,6 +143,9 @@ def run_eval1(cpu=False, bits=None, base_model='h2oai/h2ogpt-oig-oasst1-512-6_9b
                  'hyde_level': 0,
                  'hyde_template': None,
                  'doc_json_mode': False,
+                 'chatbot_role': 'Female AI Assistant',
+                 'speaker': 'SLT (female)',
+                 'tts_language': 'autodetect',
                  }
     if cpu and bits == 32:
         expected1.update({'image_audio_loaders': np.array([], dtype=object)})
