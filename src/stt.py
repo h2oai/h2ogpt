6c25bb1f2d0741aade0e61bfd8001d96bded9f3a
Jonathan C. McKinney <pseudotensor@gmail.com>
2023-11-17 04:01:35 -0800

    By default reject audio chunk if it does not lead to any new text

--- src/stt.py
+++ src/stt.py
@@ -16,7 +16,7 @@ def get_transcriber(model="openai/whisper-base.en", use_gpu=True, gpu_id='auto')
     return transcriber
 
 
-def transcribe(text0, chunks, new_chunk, transcriber=None, max_chunks=None, sst_floor=100.0, debug=False):
+def transcribe(text0, chunks, new_chunk, transcriber=None, max_chunks=None, sst_floor=100.0, reject_no_new_text=True, debug=False):
     if max_chunks is not None and len(chunks) > max_chunks:
         # refuse to update
         return chunks, text0
@@ -27,20 +27,35 @@ def transcribe(text0, chunks, new_chunk, transcriber=None, max_chunks=None, sst_
     sr, y = new_chunk
     avg = np.average(np.abs(y))
     if avg > sst_floor:
-        if debug:
-            print("Got chunk: %s" % avg, flush=True)
-        chunks = chunks + [y] if chunks else [y]
+        if debug or True:
+            print("Got possible chunk: %s" % avg, flush=True)
+        chunks_new = chunks + [y] if chunks else [y]
     else:
-        if debug:
-            print("Rejected chunk: %s" % avg, flush=True)
-    if chunks:
-        stream = np.concatenate(chunks)
+        chunks_new = chunks
+        if debug or True:
+            print("Rejected quiet chunk: %s" % avg, flush=True)
+    if chunks_new:
+        stream = np.concatenate(chunks_new)
         stream = stream.astype(np.float32)
-        stream /= np.max(np.abs(stream) + 1E-7)
-
+        max_stream = np.max(np.abs(stream) + 1E-7)
+        stream /= max_stream
         text = transcriber({"sampling_rate": sr, "raw": stream})["text"]
-        if debug:
-            print("y.shape: %s stream.shape: %s text0=%s text=%s" % (str(y.shape), str(stream.shape), text0, text))
+
+        if chunks:
+            stream0 = np.concatenate(chunks)
+            stream0 = stream0.astype(np.float32)
+            max_stream0 = np.max(np.abs(stream0) + 1E-7)
+            stream0 /= max_stream0
+            text_y = transcriber({"sampling_rate": sr, "raw": stream0})["text"]
+        else:
+            text_y = None
+
+        if debug or True:
+            print("y.shape: %s stream.shape: %s text0=%s text=%s text_y=%s" % (str(y.shape), str(stream.shape), text0, text, text_y))
+        if reject_no_new_text and (text == text_y):
+            print("Rejected non-textual chunk: %s" % avg, flush=True)
+            # if didn't generate text, reject the chunk.  E.g. when typing on keyboard that ends up being loud enough but is definitely not words.
+            chunks_new = chunks
     else:
         text = ''
-    return chunks, text0 + text
+    return chunks_new, text0 + text
