431864799147e2d7032e06e942141a75be129188
Jonathan C. McKinney <pseudotensor@gmail.com>
2023-11-26 12:28:19 -0800

    linux install script updates

--- docs/linux_install.sh
+++ docs/linux_install.sh
@@ -60,24 +60,23 @@ fi
 #* HNSW issue:
 #    In some cases old chroma migration package will install old hnswlib and that may cause issues when making a database, then do:
 #   ```bash
-   pip uninstall hnswlib==0.7.0
+   pip uninstall -y hnswlib==0.7.0
    # restore correct version
    pip install chroma-hnswlib==0.7.3
 #   ```
 #* Selenium needs to have chrome installed, e.g. on Ubuntu:
 #    ```bash
-    sudo bash
-    apt install -y unzip xvfb libxi6 libgconf-2-4
-    apt install -y default-jdk
-    curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
-    bash -c "echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' >> /etc/apt/sources.list.d/google-chrome.list"
-    apt -y update
-    apt -y install google-chrome-stable  # e.g. Google Chrome 114.0.5735.198
-    google-chrome --version  # e.g. Google Chrome 114.0.5735.198
+    sudo apt install -y unzip xvfb libxi6 libgconf-2-4
+    sudo apt install -y default-jdk
+    sudo bash -c 'curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add'
+    sudo bash -c "echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' >> /etc/apt/sources.list.d/google-chrome.list"
+    sudo apt -y update
+    sudo apt -y install google-chrome-stable  # e.g. Google Chrome 114.0.5735.198
+    sudo google-chrome --version  # e.g. Google Chrome 114.0.5735.198
     # visit https://chromedriver.chromium.org/downloads and download matching version
     # E.g.
-    wget https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip
-    unzip chromedriver_linux64.zip
+    sudo wget https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip
+    sudo unzip chromedriver_linux64.zip
     sudo mv chromedriver /usr/bin/chromedriver
     sudo chown root:root /usr/bin/chromedriver
     sudo chmod +x /usr/bin/chromedriver
