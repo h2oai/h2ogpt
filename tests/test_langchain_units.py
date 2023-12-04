74a4c4dd3f8781da9a9754ad138c42bc8b84181f
Jonathan C. McKinney <pseudotensor@gmail.com>
2023-12-04 10:17:29 -0800

    Update test

--- tests/test_langchain_units.py
+++ tests/test_langchain_units.py
@@ -1751,18 +1751,18 @@ def test_chroma_filtering():
                         rets1 = rets[0]
                         if chroma_new:
                             if answer_with_sources == -1:
-                                assert len(rets1) == 5 and (
+                                assert len(rets1) == 7 and (
                                         'h2oGPT' in rets1['response'] or 'H2O GPT' in rets1['response'] or 'H2O.ai' in
                                         rets1['response'])
                             else:
-                                assert len(rets1) == 5 and (
+                                assert len(rets1) == 7 and (
                                         'h2oGPT' in rets1['response'] or 'H2O GPT' in rets1['response'] or 'H2O.ai' in
                                         rets1['response'])
                                 if document_subset == DocumentSubset.Relevant.name:
                                     assert 'h2oGPT' in rets1['sources']
                         else:
                             if answer_with_sources == -1:
-                                assert len(rets1) == 5 and (
+                                assert len(rets1) == 7 and (
                                         'whisper' in rets1['response'].lower() or
                                         'phase' in rets1['response'].lower() or
                                         'generate' in rets1['response'].lower() or
@@ -1773,7 +1773,7 @@ def test_chroma_filtering():
                                         'gravitational' in rets1['response'].lower()
                                 )
                             else:
-                                assert len(rets1) == 5 and (
+                                assert len(rets1) == 7 and (
                                         'whisper' in rets1['response'].lower() or
                                         'phase' in rets1['response'].lower() or
                                         'generate' in rets1['response'].lower() or
