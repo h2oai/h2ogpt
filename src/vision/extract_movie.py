62cb2dd5d44e5d5fe3a6e984645b92c441a09566
Jonathan C. McKinney <pseudotensor@gmail.com>
2023-12-06 19:58:23 -0800

    Use unique download dir to avoid re-use

--- src/vision/extract_movie.py
+++ src/vision/extract_movie.py
@@ -6,6 +6,7 @@ from src.utils import makedirs, sanitize_filename
 
 def extract_unique_frames(urls=None, file=None, download_dir=None, export_dir=None, extract_frames=10):
     download_dir = download_dir or os.getenv('VID_DOWNLOADS', "viddownloads")
+    download_dir = os.path.join(download_dir, str(uuid.uuid4()))
     if urls:
         import fiftyone.utils.youtube as fouy
         fouy.download_youtube_videos(urls, download_dir=download_dir)
