3ff7edd83778bf7e71847f4e3d9907ad00bb006a
Jonathan C. McKinney <pseudotensor@gmail.com>
2023-12-23 22:03:33 -0800

    update test

--- tests/test_client_calls.py
+++ tests/test_client_calls.py
@@ -4098,7 +4098,8 @@ def check_curl_plain_api():
     response = requests.post('http://127.0.0.1:7860/api/submit_nochat_plain_api', headers=headers, json=json_data)
     res_dict = ast.literal_eval(json.loads(response.content.decode(encoding='utf-8', errors='strict'))['data'][0])
 
-    assert 'assistant' in res_dict['response'] or 'computer program' in res_dict['response']
+    assert 'assistant' in res_dict['response'] or 'computer program' in res_dict['response'] or 'program designed' in \
+           res_dict['response']
     assert 'Who are you?' in res_dict['prompt_raw']
     assert 'llama' == res_dict['save_dict']['base_model'] or 'HuggingFaceH4/zephyr-7b-beta' == res_dict['save_dict'][
         'base_model']
