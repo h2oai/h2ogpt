222a09015bbd04575de7d964d7512deb85ef0a0f
Jonathan C. McKinney <pseudotensor@gmail.com>
2024-01-06 01:33:18 -0800

    More prompts

--- src/prompter.py
+++ src/prompter.py
@@ -1698,12 +1698,15 @@ def get_llava_prompts():
             ]
 
 
-def get_verifier_prompt(instruction, response, reference_answer, criteria_description,
-                        score1_description,
-                        score2_description,
-                        score3_description,
-                        score4_description,
-                        score5_description):
+def get_response_verification_prompt(instruction,
+                                     response,
+                                     reference_answer,
+                                     criteria_description,
+                                     score1_description,
+                                     score2_description,
+                                     score3_description,
+                                     score4_description,
+                                     score5_description):
     # https://huggingface.co/kaist-ai/prometheus-13b-v1.0
 
     task_description = """###Task Description:
@@ -1741,7 +1744,7 @@ Score 5: The model flawlessly identifies the emotional context of the user's inp
 ###Feedback:
 """
 
-    prompt = f"""###Task Description:
+    return f"""###Task Description:
 {task_description}
 
 ###The instruction to evaluate:
@@ -1762,4 +1765,121 @@ Score 4: {score4_description}
 Score 5: {score5_description}
 
 ###Feedback: """
-    return prompt
+
+
+def get_correctness_eval_verification_prompt(query,
+                                             response,
+                                             answer,
+                                             ):
+    return f"""###Task Description: An instruction (might include an Input inside it), a query, a response to evaluate, a reference answer that gets a score of 5, and a score rubric representing a evaluation criteria are given.
+1. Write a detailed feedback that assesses the quality of the response strictly based on the given score rubric, not evaluating in general.
+2. After writing a feedback, write a score that is either 1 or 2 or 3 or 4 or 5. You should refer to the score rubric.
+3. The output format should look as follows: 'Feedback: (write a feedback for criteria) [RESULT] (1 or 2 or 3 or 4 or 5)'
+4. Please do not generate any other opening, closing, and explanations.
+5. Only evaluate on common things between generated answer and reference answer. Don't evaluate on things which are present in reference answer but not in generated answer.
+
+###The instruction to evaluate: Your task is to evaluate the generated answer and reference answer for the query: {query}
+
+###Generate answer to evaluate: {response}
+
+###Reference Answer (Score 5): {answer}
+
+###Score Rubrics:
+Score 1: If the generated answer is not relevant to the user query and reference answer.
+Score 2: If the generated answer is according to reference answer but not relevant to user query.
+Score 3: If the generated answer is relevant to the user query and reference answer but contains mistakes.
+Score 4: If the generated answer is relevant to the user query and has the exact same metrics as the reference answer, but it is not as concise.
+Score 5: If the generated answer is relevant to the user query and fully correct according to the reference answer.
+
+###Feedback:"""
+
+
+def get_faithfulness_eval_verification_prompt(information,
+                                              context,
+                                              ):
+    return f"""###Task Description: An instruction (might include an Input inside it), an information, a context, and a score rubric representing evaluation criteria are given.
+1. You are provided with evaluation task with the help of information, context information to give result based on score rubrics.
+2. Write a detailed feedback based on evaluation task and the given score rubric, not evaluating in general.
+3. After writing a feedback, write a score that is YES or NO. You should refer to the score rubric.
+4. The output format should look as follows: "Feedback: (write a feedback for criteria) [RESULT] (YES or NO)?
+5. Please do not generate any other opening, closing, and explanations.
+
+###The instruction to evaluate: Your task is to evaluate if the given piece of information is supported by context.
+
+###Information: {information}
+
+###Context: {context}
+
+###Score Rubrics:
+Score YES: If the given piece of information is supported by context.
+Score NO: If the given piece of information is not supported by context
+
+###Feedback: """
+
+
+def get_faithfulness_refine_verification_prompt(information,
+                                                answer,
+                                                context,
+                                                ):
+    return f"""###Task Description: An instruction (might include an Input inside it), a information, a context information, an existing answer, and a score rubric representing a evaluation criteria are given.
+1. You are provided with evaluation task with the help of information, context information and an existing answer.
+2. Write a detailed feedback based on evaluation task and the given score rubric, not evaluating in general.
+3. After writing a feedback, write a score that is YES or NO. You should refer to the score rubric.
+4. The output format should look as follows: "Feedback: (write a feedback for criteria) [RESULT] (YES or NO)"
+5. Please do not generate any other opening, closing, and explanations.
+
+###The instruction to evaluate: If the information is present in the context and also provided with an existing answer.
+
+###Existing answer: {answer}
+
+###Information: {information}
+
+###Context: {context}
+
+###Score Rubrics:
+Score YES: If the existing answer is already YES or If the Information is present in the context.
+Score NO: If the existing answer is NO and If the Information is not present in the context.
+
+###Feedback: """
+
+
+def get_relevancy_eval_prompt(query_and_response, context):
+    return f"""###Task Description: An instruction (might include an Input inside it), a query with response, context, and a score rubric representing evaluation criteria are given.
+1. You are provided with evaluation task with the help of a query with response and context.
+2. Write a detailed feedback based on evaluation task and the given score rubric, not evaluating in general.
+3. After writing a feedback, write a score that is YES or NO. You should refer to the score rubric.
+4. The output format should look as follows: "Feedback: (write a feedback for criteria) [RESULT] (YES or NO)?
+5. Please do not generate any other opening, closing, and explanations.
+
+###The instruction to evaluate: Your task is to evaluate if the response for the query is in line with the context information provided.
+
+###Query and Response: {query_and_response}
+
+###Context: {context}
+
+###Score Rubrics:
+Score YES: If the response for the query is in line with the context information provided.
+Score NO: If the response for the query is not in line with the context information provided.
+
+###Feedback: """
+
+
+def get_relevancy_refine_prompt(query_str, context_str):
+    return f"""###Task Description: An instruction (might include an Input inside it), a query with response, context, an existing answer, and a score rubric representing a evaluation criteria are given.
+1. You are provided with evaluation task with the help of a query with response and context and an existing answer.
+2. Write a detailed feedback based on evaluation task and the given score rubric, not evaluating in general.
+3. After writing a feedback, write a score that is YES or NO. You should refer to the score rubric.
+4. The output format should look as follows: "Feedback: (write a feedback for criteria) [RESULT] (YES or NO)"
+5. Please do not generate any other opening, closing, and explanations.
+
+###The instruction to evaluate: Your task is to evaluate if the response for the query is in line with the context information provided.
+
+###Query and Response: {query_str}
+
+###Context: {context_str}
+
+###Score Rubrics:
+Score YES: If the existing answer is already YES or If the response for the query is in line with the context information provided.
+Score NO: If the existing answer is NO and If the response for the query is in line with the context information provided.
+
+###Feedback: """
