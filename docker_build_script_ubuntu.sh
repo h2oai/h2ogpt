7360a25d1a9da8b5476b7b3dea885e1275703e10
Jonathan C. McKinney <pseudotensor@gmail.com>
2024-01-26 20:51:33 -0800

    more builds

--- docker_build_script_ubuntu.sh
+++ docker_build_script_ubuntu.sh
@@ -86,7 +86,9 @@ python -m venv vllm_env --system-site-packages
 #/h2ogpt_conda/vllm_env/bin/python -m pip install https://github.com/vllm-project/vllm/releases/download/v0.2.7/vllm-0.2.7+cu118-cp310-cp310-manylinux1_x86_64.whl
 #/h2ogpt_conda/vllm_env/bin/python -m pip install vllm
 
-/h2ogpt_conda/vllm_env/bin/python -m pip install mosaicml-turbo==0.0.9 megablocks==0.5.1 triton==2.2.0 ray pandas gputil==1.4.0 fschat==0.2.34 flash-attn==2.4.2 autoawq==0.1.8 uvicorn[standard]
+/h2ogpt_conda/vllm_env/bin/python -m pip install mosaicml-turbo==0.0.9 megablocks==0.5.1 ray pandas gputil==1.4.0 fschat==0.2.34 flash-attn==2.4.2 autoawq==0.1.8 uvicorn[standard]
+/h2ogpt_conda/vllm_env/bin/python -m pip install https://h2o-release.s3.amazonaws.com/h2ogpt/megablocks-0.5.1-cp310-cp310-linux_x86_64.whl
+/h2ogpt_conda/vllm_env/bin/python -m pip install https://h2o-release.s3.amazonaws.com/h2ogpt/triton-2.2.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
 /h2ogpt_conda/vllm_env/bin/python -m pip install https://h2o-release.s3.amazonaws.com/h2ogpt/vllm-0.2.7%2Bcu123-cp310-cp310-linux_x86_64.whl
 mkdir $VLLM_CACHE
 chmod -R a+rwx /h2ogpt_conda
