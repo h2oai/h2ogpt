93531b9af2a474503a37442e3f96577d20ae14d2
Jonathan C. McKinney <pseudotensor@gmail.com>
2024-02-03 17:20:49 -0800

    Faster transfers

--- src/gen.py
+++ src/gen.py
@@ -22,6 +22,7 @@ from requests.exceptions import ReadTimeout as ReadTimeout2
 if os.path.dirname(os.path.abspath(__file__)) not in sys.path:
     sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
+os.environ['HF_HUB_ENABLE_HF_TRANSFER'] = '1'
 os.environ['HF_HUB_DISABLE_TELEMETRY'] = '1'
 os.environ['BITSANDBYTES_NOWELCOME'] = '1'
 warnings.filterwarnings('ignore', category=UserWarning, message='TypedStorage is deprecated')
