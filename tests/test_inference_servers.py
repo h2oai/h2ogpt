6de54ffd23fff37687bafa61594e198ea0a41732
Jonathan C. McKinney <pseudotensor@gmail.com>
2024-02-04 19:38:41 -0800

    Fix test

--- tests/test_inference_servers.py
+++ tests/test_inference_servers.py
@@ -449,13 +449,13 @@ def test_hf_inference_server(base_model, force_langchain_evaluate, do_langchain,
             assert 'bird' in ret2['response']
             assert 'bird' in ret3['response']
             assert 'year old student from the' in ret4['response'] or 'I am a person who is asking you a question' in \
-                   ret4['response'] or 'year old' in ret4['response']
+                   ret4['response'] or 'year old' in ret4['response'] or 'I am an AI language model' in ret4['response']
             assert 'year old student from the' in ret5['response'] or 'I am a person who is asking you a question' in \
-                   ret5['response'] or 'year old' in ret5['response']
+                   ret5['response'] or 'year old' in ret5['response'] or 'I am an AI language model' in ret5['response']
             assert 'year old student from the' in ret6['response'] or 'I am a person who is asking you a question' in \
-                   ret6['response'] or 'year old' in ret6['response']
+                   ret6['response'] or 'year old' in ret6['response'] or 'I am an AI language model' in ret6['response']
             assert 'year old student from the' in ret7['response'] or 'I am a person who is asking you a question' in \
-                   ret7['response'] or 'year old' in ret7['response']
+                   ret7['response'] or 'year old' in ret7['response'] or 'I am an AI language model' in ret7['response']
         elif base_model == 'h2oai/h2ogpt-oig-oasst1-512-6_9b':
             assert 'h2oGPT' in ret1['response']
             assert 'Birds' in ret2['response']
