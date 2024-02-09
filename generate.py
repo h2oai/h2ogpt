a7bc7f269e71b83e5341a902d72d1657dd2801cd
Jonathan C. McKinney <pseudotensor@gmail.com>
2024-02-09 15:40:13 -0800

    Protect stdout/stderr from packaegs closing and redirecting that leads to I/O operation on closed file in multi-threaded environment

--- generate.py
+++ generate.py
@@ -4,6 +4,10 @@ import sys
 if os.path.dirname(os.path.abspath(__file__)) not in sys.path:
     sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
+from src.utils_sys import protect_stdout_stderr
+
+protect_stdout_stderr()
+
 from src.gen import main
 from src.utils import H2O_Fire
 
