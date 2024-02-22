aab91132880f0604f05ea01cadbbd43c17fbaa12
Jonathan C. McKinney <pseudotensor@gmail.com>
2024-02-21 20:32:57 -0800

    Groq via OpenAI API

--- src/gen.py
+++ src/gen.py
@@ -572,6 +572,11 @@ def main(
                                  vllm_chat:https://vllm.h2o.ai:5001:/1b1219f7-4bb4-43e9-881f-fa8fa9fe6e04/v1:1234ABCD
                                  where vllm.h2o.ai is the DNS name of the IP, 5001 is the port, /1b1219f7-4bb4-43e9-881f-fa8fa9fe6e04/v1 is the url of the "page" to access, and 1234ABCD is the api key
 
+                              Or for groq, can use OpenAI API like:
+                              vllm:https://api.groq.com/openai:None:/v1:<api key>'
+                              with: other model_lock or CLI options: {'base_model':'mixtral-8x7b-32768', 'visible_models':'mixtral-8x7b-32768', 'max_seq_len': 31744, 'prompt_type':'plain'}
+                              i.e.ensure to use 'plain' prompt, not mixtral.
+
                              Or Address can be replicate:
                              Use:
                               --inference_server=replicate:<model name string> will use a Replicate server, requiring a Replicate key.
