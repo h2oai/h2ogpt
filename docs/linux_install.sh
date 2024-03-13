59125281d2a601d26ea30bdbfe706afe3dca39bb
Jonathan C. McKinney <pseudotensor@gmail.com>
2024-03-13 00:28:38 -0700

    Update fiftyone and update patches to pytube for video handling

--- docs/linux_install.sh
+++ docs/linux_install.sh
@@ -213,24 +213,7 @@ pip install -r reqs_optional/requirements_optional_agents.txt -c reqs_optional/r
 #  For more info see [SERP Docs](README_SerpAPI.md).
 
 
-#
-#* Deal with not-thread-safe things in LangChain:
-#
-sp=`python3.10 -c 'import site; print(site.getsitepackages()[0])'`
-sed -i  's/with HiddenPrints():/if True:/g' $sp/langchain_community/utilities/serpapi.py
-#sed -i 's/"progress": Status.PROGRESS,/"progress": Status.PROGRESS,\n            "heartbeat": Status.PROGRESS,/g' gradio_client/utils.py
-#sed -i 's/async for line in response.aiter_text():/async for line in response.aiter_lines():\n                if len(line) == 0:\n                    continue\n                if line == """{"detail":"Not Found"}""":\n                    continue/g' gradio_client/utils.py
-
-
-# fix pytube to avoid errors for restricted content
-sed -i "s/client='ANDROID_MUSIC'/client='ANDROID'/g" $sp/pytube/innertube.py
-
-# fix asyncio same way websockets was fixed, else keep hitting errors in async calls
-# https://github.com/python-websockets/websockets/commit/f9fd2cebcd42633ed917cd64e805bea17879c2d7
-sed -i "s/except OSError:/except (OSError, RuntimeError):/g" $sp/anyio/_backends/_asyncio.py
-
-# https://github.com/gradio-app/gradio/issues/7086
-sed -i 's/while True:/while True:\n            time.sleep(0.001)\n/g' $sp/gradio_client/client.py
+bash ./run_patches.sh
 
 
 #
