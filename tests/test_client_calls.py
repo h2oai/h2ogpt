b300900a47e0fe539b1dc45b2b7bf6ad37e6a66b
Jonathan C. McKinney <pseudotensor@gmail.com>
2024-03-13 00:02:36 -0700

    Update test

--- tests/test_client_calls.py
+++ tests/test_client_calls.py
@@ -2774,7 +2774,7 @@ def test_client_chat_stream_langchain_openai_embeddings():
     from src.gpt_langchain import load_embed
     got_embedding, use_openai_embedding, hf_embedding_model = load_embed(persist_directory='db_dir_UserData')
     assert use_openai_embedding
-    assert hf_embedding_model == 'hkunlp/instructor-large'  # but not used
+    assert hf_embedding_model in ['', 'hkunlp/instructor-large']  # but not used
     assert got_embedding
 
 
