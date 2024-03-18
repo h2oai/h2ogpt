96626764bc697f4785f9a667cc145238290c1f17
Jonathan C. McKinney <pseudotensor@gmail.com>
2024-03-18 10:07:47 -0700

    Fix permission denied error if file already exists

--- docs/linux_install.sh
+++ docs/linux_install.sh
@@ -125,6 +125,7 @@ fi
 
 # upgrade chrome to latest
 sudo mkdir -p /etc/apt/keyrings/
+sudo rm -rf /tmp/google.pub
 sudo wget https://dl-ssl.google.com/linux/linux_signing_key.pub -O /tmp/google.pub
 sudo gpg --no-default-keyring --keyring /etc/apt/keyrings/google-chrome.gpg --import /tmp/google.pub
 sudo echo 'deb [arch=amd64 signed-by=/etc/apt/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
