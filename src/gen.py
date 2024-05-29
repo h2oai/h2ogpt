32f6625fafdd70a4f386090222bd91144337ecf0
Jonathan C. McKinney <pseudotensor@gmail.com>
2024-05-29 00:59:22 -0700

    Fix together.ai

--- src/gen.py
+++ src/gen.py
@@ -4847,7 +4847,12 @@ def evaluate(
                     if inf_type == 'vllm_chat':
                         # https://github.com/InternLM/lmdeploy/blob/e6468e7afda6b29d4c065f296a4e893b52bd33d5/lmdeploy/serve/proxy/proxy.py#L320
                         # https://lmdeploy.readthedocs.io/en/latest/serving/api_server.html#restful-api
-                        model_name = openai_client.models.list().data[0].id
+                        try:
+                            model_name = openai_client.models.list().data[0].id
+                        except Exception as e:
+                            print("Failed to get model name from OpenAI client, using default", e)
+                            # together.ai
+                            model_name = base_model
                     else:
                         model_name = base_model
                     responses = openai_client.chat.completions.create(
