ce7b63930323595dbba511505301b5df86b97136
Jonathan C. McKinney <pseudotensor@gmail.com>
2024-06-04 18:53:16 -0700

    pattern stuff worse than repair_json

--- src/version.py
+++ src/version.py
@@ -1,1 +1,5 @@
-__version__ = "ab69eb929cf6e81aa010744d392ff3874fcd7645"
+<<<<<<< Updated upstream
+__version__ = "6449072c7c666d105ac99e586158c0688ffb5916"
+=======
+__version__ = "6449072c7c666d105ac99e586158c0688ffb5916"
+>>>>>>> Stashed changes
