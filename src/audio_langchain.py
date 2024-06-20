e964c970ac9b693070863c2cc8fdaf66f4ba4dbe
Jonathan C. McKinney <pseudotensor@gmail.com>
2024-06-19 23:29:37 -0700

    Update import

--- src/audio_langchain.py
+++ src/audio_langchain.py
@@ -416,7 +416,7 @@ class H2OAudioCaptionLoader(ImageCaptionLoader):
 
 from typing import Iterable, List
 
-from langchain.document_loaders.blob_loaders import FileSystemBlobLoader
+from langchain_community.document_loaders import FileSystemBlobLoader
 from langchain.document_loaders.blob_loaders.schema import Blob, BlobLoader
 
 
--- src/version.py
+++ src/version.py
@@ -1,1 +1,1 @@
-__version__ = "4f8afc4cdf84d5c88954bf287d2f2da2afb0e539"
+__version__ = "65f77a21fae92cf145fa2ee35636de83cb9184e8"
