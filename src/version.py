58bb6180ac7c1c5526e4a0fde01564b8c95c354e
Jonathan C. McKinney <pseudotensor@gmail.com>
2024-09-03 13:21:18 -0700

    Clean-up agent files by isolating different concerns into different files

--- src/version.py
+++ src/version.py
@@ -1,1 +1,5 @@
-__version__ = "a62aeaa85ad2861869ef648eae090787775d4c88"
+<<<<<<< Updated upstream
+__version__ = "bcb06fdbd0c050c18277979ac5060131ffd4063e"
+=======
+__version__ = "bcb06fdbd0c050c18277979ac5060131ffd4063e"
+>>>>>>> Stashed changes
