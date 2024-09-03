fb76eb858d6ae106e4c3f1cb0919045c93d254c3
Jonathan C. McKinney <pseudotensor@gmail.com>
2024-09-03 13:35:44 -0700

    Fix version

--- src/version.py
+++ src/version.py
@@ -1,5 +1,1 @@
-<<<<<<< Updated upstream
-__version__ = "cbf22b23bd078b6cbe78a12fecde797ba01fe9b3"
-=======
-__version__ = "cbf22b23bd078b6cbe78a12fecde797ba01fe9b3"
->>>>>>> Stashed changes
+__version__ = "a5bfdac6517402f827784646cd90314eaa74c5c7"
