742c0937aba08f3d6f7ea49dccf4025a3cde3526
fatihozturkh2o <fatih.ozturk@h2o.ai>
2024-09-25 08:46:28 -0700

    Update <no_code_executed> message

--- openai_server/autogen_utils.py
+++ openai_server/autogen_utils.py
@@ -386,8 +386,12 @@ os.environ['TERM'] = 'dumb'
 * This is expected if you had code blocks but they were not meant for python or shell execution.
 * For example, you may have shown code for demonstration purposes.
 * If you intended to execute code, be sure to add the comment: # execution: true and try again.
-* Otherwise, if no code execution was expected, then do not respond or react to this "no_code_execution" text and instead just directly and immediately summarize the actual answer to the user's original question and then TERMINATE.
-* Do not thank the user for telling you that the code was not executed.
+* Otherwise, if no code execution was expected: 
+   - Do not respond or react to this "no_code_execution" text. 
+   - Instead just directly and immediately provide the actual answer to the user's original question.
+   - The user will only see your final message, that's why make sure to include the answer that the user is looking for in your final message.
+   - Do not thank the user for telling you that the code was not executed.
+   - If there is no more task left, TERMINATE the chat.
 </no_code_executed_notes>
 """)
         except Exception as e:
@@ -758,11 +762,10 @@ def terminate_message_func(msg):
     # in conversable agent, roles are flipped relative to actual OpenAI, so can't filter by assistant
     #        isinstance(msg.get('role'), str) and
     #        msg.get('role') == 'assistant' and
-
     has_message = isinstance(msg, dict) and isinstance(msg.get('content', ''), str)
-    has_term = has_message and msg.get('content', '').endswith("TERMINATE") or msg.get('content', '') == ''
-    has_execute = has_message and '# execution: true' in msg.get('content', '')
-
+    has_no_code_executed_message = has_message and "<no_code_executed>" in msg.get('content', '')
+    has_term = has_message and "TERMINATE" in msg.get('content', '') and not has_no_code_executed_message or msg.get('content', '') == ''
+    has_execute = has_message and '# execution: true' in msg.get('content', '') and not has_no_code_executed_message
     if has_execute:
         # sometimes model stops without verifying results if it dumped all steps in one turn
         # force it to continue
