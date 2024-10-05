4ec614a0a374c8f9a9e7a8eb8465acb5e1ca8839
Ikko Eltociear Ashimine <eltociear@gmail.com>
2024-10-06 00:03:32 +0900

    chore: update llm_exllama.py
    occassionally -> occasionally

--- src/llm_exllama.py
+++ src/llm_exllama.py
@@ -308,7 +308,7 @@ class Exllama(LLM):
         ####    text_callback(prompt)
         text = ""
         while (generator.gen_num_tokens() <= (
-                self.max_seq_len - 4)):  # Slight extra padding space as we seem to occassionally get a few more than 1-2 tokens
+                self.max_seq_len - 4)):  # Slight extra padding space as we seem to occasionally get a few more than 1-2 tokens
             # Fetch a token
             token = token_getter()
 
