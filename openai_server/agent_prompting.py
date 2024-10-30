3b2288d420309d4cd2e012f8e2400f31ace50ed3
Jonathan C. McKinney <pseudotensor@gmail.com>
2024-10-30 00:59:18 -0700

    Avoid quotes

--- openai_server/agent_prompting.py
+++ openai_server/agent_prompting.py
@@ -613,6 +613,7 @@ python {cwd}/openai_server/agent_tools/convert_document_to_text.py [--files FILE
 * Zip files will be extracted and each file inside will be converted to text.
 * The convert_document_to_text.py tool can be many url(s) (http://www.cnn.com, https://aiindex.stanford.edu/wp-content/uploads/2024/04/HAI_2024_AI-Index-Report.pdf, youtube videos, etc.) to convert to text for other tools.
 * The convert_document_to_text.py tool cannot be used for images or videos.
+* Note, to avoid escaping special characters, put your files or URLs in quotes.
 * However, use convert_document_to_text.py if just want to directly ask a question about a non-image document or URL.
 * However, use ask_question_about_image.py if just want to directly ask a question about an image.
 * For data analysis on xlsx or xls files, you must use non-text ways like pd.read_excel().
--- src/version.py
+++ src/version.py
@@ -1,1 +1,1 @@
-__version__ = "abfd014ef6b98b08ecb4ad513db388500f2d11aa"
+__version__ = "de14a5e20c52f4cf75be21f21d055c06f78f23c7"
